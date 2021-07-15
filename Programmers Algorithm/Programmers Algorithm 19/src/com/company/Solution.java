73c20b560cb008f5908a3d0b728495a3554eccab
rere950303 <yhwjjang1995@naver.com>
2021-07-15 13:34:23 +0900

    feat: Modify annotation

--- Programmers Algorithm/Programmers Algorithm 19/src/com/company/Solution.java
+++ Programmers Algorithm/Programmers Algorithm 19/src/com/company/Solution.java
@@ -1,3 +1,9 @@
+//
+//  Programmers Algorithm 19(체육복)
+//  https://programmers.co.kr/learn/courses/30/lessons/42862
+//  Created by hyungwook on 2021/07/14.
+//
+
 package com.company;
 
 import java.util.Arrays;
